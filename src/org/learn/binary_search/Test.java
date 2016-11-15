e7307860a20e396783db43106aa44bf4c8d17529
张鹿鹿 <zhanglulu1007@foxmail.com>
2016-11-15 09:51:55 +0800

    Initial commit

---
+++ src/org/learn/binary_search/Test.java
@@ -0,0 +1,38 @@
+package org.learn.binary_search;
+
+import java.util.Arrays;
+
+/**
+ * Created by lulu on 2016/11/15.
+ */
+
+//二分搜索
+public class Test {
+    public static void main(String[] args) {
+        int[] nums = {20, 34, 30, 45, 54, 60};
+        // 先进行排序
+        Arrays.sort(nums);
+        System.out.println(binarySearch(nums, 54));
+
+    }
+
+    //已经排好序的数组
+    public static int binarySearch(int[] nums, int key) {
+
+        int start = 0;
+        int end = nums.length - 1;
+        int mid = -1;
+
+        while (start <= end) {
+            mid = (start + end) / 2;
+            if (nums[mid] == key) {
+                return mid;//已经查到返回！
+            } else if (nums[mid] > key) {
+                end = mid - 1;
+            } else if (nums[mid] < key) {
+                start = mid + 1;
+            }
+        }
+        return -1;
+    }
+}
---
+++ src/org/learn/sort/Test.java
@@ -0,0 +1,8 @@
+package org.learn.sort;
+
+/**
+ * Created by lulu on 2016/11/15.
+ */
+//整合常用的的搜索算法
+public class Test {
+}
